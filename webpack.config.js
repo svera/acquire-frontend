6d5148e84cc2cd4f72594975eac75b382ca5e056
Sergio Vera Castellano <severa@magento.com>
2017-07-31 14:45:28 +0200

    Upgraded to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
 
-var BUILD_DIR = path.resolve(__dirname, 'build');
-var APP_DIR = path.resolve(__dirname, 'frontend');
+var BUILD_DIR = path.resolve(__dirname, "build");
+var APP_DIR = path.resolve(__dirname, "frontend");
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var config = {
-  devtool: 'eval-source-map', 
-  entry: APP_DIR + '/app.jsx',
+  devtool: "eval-source-map", 
+  entry: APP_DIR + "/app.jsx",
   output: {
     path: BUILD_DIR,
     publicPath: "/",
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
   module : {
-    loaders : [
+    rules : [
       {
         test : /\.jsx?/,
+        loader: "babel-loader",
         include : APP_DIR,
-        loader : 'babel'
       },
       {
-          test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-          loader: 'url-loader'
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        loader: "url-loader"
       },
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
+        use: [
+          "style-loader", "css-loader", "sass-loader"
+        ]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template/index.ejs'
+      template: "template/index.ejs"
     })       
   ]    
 };
