290e2b998c5521051f5e2c60da3144a9742185cf
Sergio Vera Castellano <severa@magento.com>
2017-08-02 09:19:24 +0200

    Added buffer for reordering incoming messages

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -20,7 +20,6 @@ class App extends React.Component {
       super(props);
       this.state = {
         screen: HOME,
-        game: '',
         gameID: '',
         players: [],
         status: null,
@@ -51,105 +50,131 @@ class App extends React.Component {
       }
 
       this.t = this.t.bind(this);
+  }
+
+  initLanguages() {
+    if (!localStorage.getItem('language')) {
+      localStorage.setItem('language', 'en');
     }
+    this.polyglot = new Polyglot();
+    this.polyglot.extend(en);
+    this.polyglot.extend(es);
+    return localStorage.getItem('language');
+  }
 
-    initLanguages() {
-      if (!localStorage.getItem('language')) {
-        localStorage.setItem('language', 'en');
-      }
-      this.polyglot = new Polyglot();
-      this.polyglot.extend(en);
-      this.polyglot.extend(es);
-      return localStorage.getItem('language');
-    }
+  // Return the translation for the passed key in the current language
+  t(key, values) {
+    return this.polyglot.t(localStorage.getItem('language') + "." + key, values);
+  }
 
-    // Return the translation for the passed key in the current language
-    t(key, values) {
-      return this.polyglot.t(localStorage.getItem('language') + "." + key, values);
-    }
+  parseMessage(data) {
+    var wrapped = JSON.parse(data);
+    var msg = wrapped.cnt;
+    
+    switch (wrapped.typ) {
+      case "out":
+        if (msg.rea == 'ter') {
+          sessionStorage.setItem('info', this.t("game_terminated"));
+        }
+        if (msg.rea == 'kck') {
+          sessionStorage.setItem('info', this.t("kicked"));
+        }
+        if (msg.rea == 'tim') {
+          sessionStorage.setItem('info', this.t("room_timed_out"));
+        }
+        if (msg.rea == 'ptm') {
+          sessionStorage.setItem('info', this.t("player_timeout"));
+        }
+        if (msg.rea == 'pan') {
+          sessionStorage.setItem('info', this.t("game_panicked"));
+        }
+        sessionStorage.setItem('role', '');
+        this.setState({
+          screen: HOME,
+          error: ''
+        });
+        break;
 
-    parseMessage(data) {
-      var wrapped = JSON.parse(data);
-      var msg = wrapped.cnt;
-      
-      switch (wrapped.typ) {
-        case "out":
-          if (msg.rea == 'ter') {
-            sessionStorage.setItem('info', this.t("game_terminated"));
-          }
-          if (msg.rea == 'kck') {
-            sessionStorage.setItem('info', this.t("kicked"));
-          }
-          if (msg.rea == 'tim') {
-            sessionStorage.setItem('info', this.t("room_timed_out"));
-          }
-          if (msg.rea == 'ptm') {
-            sessionStorage.setItem('info', this.t("player_timeout"));
-          }
-          if (msg.rea == 'pan') {
-            sessionStorage.setItem('info', this.t("game_panicked"));
-          }
-         sessionStorage.setItem('role', '');
-          this.setState({
-            screen: HOME,
-            error: ''
-          });
-          break;
+      case "err":
+        this.setState({
+          error: msg.cnt,
+        });
+        break;
 
-        case "err":
-          this.setState({
-            error: msg.cnt,
-          });
-          break;
+      case "rms":
+        this.setState({
+          rooms: msg.val
+        })
+        break;
 
-        case "rms":
-          this.setState({
-            rooms: msg.val
-          })
-          break;
-
-        case "pls":
-          if (this.state.screen == HOME || this.state.screen == LOBBY) {
-            this.setState({
-              screen: LOBBY,
-              players: msg.val
-            });
-          }
-          break;
-
-        case "joi":
-          if (msg.own) {
-            sessionStorage.setItem('role', 'mng');
-          }
-          sessionStorage.setItem('clientNumber', msg.num);
-          if (localStorage.getItem('clientName') == "") {
-            localStorage.setItem('clientName', 'Player '+msg.num);
-          }
+      case "pls":
+        if (this.state.screen == HOME || this.state.screen == LOBBY) {
           this.setState({
             screen: LOBBY,
-            gameID: msg.id
+            players: msg.val
           });
-          this.expectedStatusSequenceNumber = 1;   
-          break;
+        }
+        break;
 
-        case "upd":
-          if (wrapped.seq > this.expectedStatusSequenceNumber) {
-            console.log("Received out of order update, expecting " + this.expectedStatusSequenceNumber + ", got " + wrapped.seq);
-            this.expectedStatusSequenceNumber = wrapped.seq + 1;
-          } else if (wrapped.seq < this.expectedStatusSequenceNumber){
-            return;
-          } else {
-            this.expectedStatusSequenceNumber++;
-          }
-          this.setState({
-            screen: GAME,
-            game: 'acquire',
-            status: msg,
-            error: '',
-          });
-          break;
-      }
+      case "joi":
+        if (msg.own) {
+          sessionStorage.setItem('role', 'mng');
+        }
+        sessionStorage.setItem('clientNumber', msg.num);
+        if (localStorage.getItem('clientName') == "") {
+          localStorage.setItem('clientName', 'Player '+msg.num);
+        }
+        this.setState({
+          screen: LOBBY,
+          gameID: msg.id
+        });
+        this.expectedStatusSequenceNumber = 1;
+        this.updateBuffer = [];
+
+        break;
+
+      case "upd":
+        if (wrapped.seq > this.expectedStatusSequenceNumber) {
+          console.log("Received out of order update, expecting " + this.expectedStatusSequenceNumber + ", got " + wrapped.seq);
+          this.updateBuffer.push(wrapped);
+          return
+        }
+        this.setState({
+          screen: GAME,
+          status: msg,
+          error: '',
+        });
+        this.expectedStatusSequenceNumber++;
+
+        if (this.updateBuffer.length > 0) {
+          this.applyPendingUpdates();
+        }
+        
+        break;
     }
+  }
+
+  applyPendingUpdates() {
+    // Sort pending updates by sequence number
+    this.updateBuffer.sort((a,b) => a.seq - b.seq);
+
+    // Apply pending updates
+    for (var i = 0; i < this.updateBuffer.length; i++) {
+      if (this.updateBuffer[i].seq != this.expectedStatusSequenceNumber) {
+        return;
+      }
+
+      this.setState({
+        screen: GAME,
+        status: this.updateBuffer[i].cnt,
+        error: '',
+      });
+
+      // Remove applied update from buffer
+      this.updateBuffer.splice(i, 1);
+      this.expectedStatusSequenceNumber++;
+    }
+  }
 
   render() {
     switch (this.state.screen) {
